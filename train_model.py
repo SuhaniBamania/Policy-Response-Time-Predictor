c68c71bae32005e172426335b9c4c3962268a38a
SuhaniBamania <suhani.bamania@gmail.com>
2025-08-15 21:13:44 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-import lightgbm as lgb # Using the better model
+import lightgbm as lgb 
 import joblib
 
 # --- Configuration ---
